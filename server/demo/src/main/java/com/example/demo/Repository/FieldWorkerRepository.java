37e2b57b9a4347df6b606315c9035a2717d2165f
mukul1357 <Mukul.Gupta@iiitb.ac.in>
2024-04-18 13:03:27 +0530

    Supervisor API's updated

--- server/demo/src/main/java/com/example/demo/Repository/FieldWorkerRepository.java
+++ server/demo/src/main/java/com/example/demo/Repository/FieldWorkerRepository.java
@@ -4,5 +4,8 @@ import com.example.demo.Entity.FieldWorker;
 import com.example.demo.Entity.Supervisor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FieldWorkerRepository extends JpaRepository<FieldWorker, String> {
+    List<FieldWorker> findByArea(String area);
 }
--- server/demo/src/main/java/com/example/demo/services/SupervisorService.java
+++ server/demo/src/main/java/com/example/demo/services/SupervisorService.java
@@ -2,15 +2,18 @@ package com.example.demo.services;
 
 import com.example.demo.Entity.FieldWorker;
 import com.example.demo.Entity.Supervisor;
+import com.example.demo.Entity.User;
 import com.example.demo.Repository.FieldWorkerRepository;
+import com.example.demo.Repository.IdMappingRepository;
 import com.example.demo.Repository.SupervisorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class SupervisorService {
@@ -25,6 +28,9 @@ public class SupervisorService {
 
     @Autowired
     private DistrictService districtService;
+
+    @Autowired
+    private IdMappingRepository idMappingRepository;
 
 
     public Supervisor getDetails(String id){
@@ -52,10 +58,10 @@ public class SupervisorService {
     }
 
 
-    public boolean removeFieldWorker(String id)
+    public boolean removeFieldWorker(int fw_id)
     {
+        String id = idMappingRepository.findById(fw_id).get().getPrivateId().toString();
         Optional<FieldWorker> fieldWorkerOptional = fieldWorkerRepository.findById(id);
-
         // Delete the field worker
         if(fieldWorkerOptional.isPresent()) {
             fieldWorkerRepository.deleteById(id);
@@ -107,5 +113,23 @@ public class SupervisorService {
         } else {
             return null;
         }
+    }
+
+    public ResponseEntity<List<Map<String, String>>> findfwByArea(String area) {
+        List<FieldWorker> fieldWorkers = fieldWorkerRepository.findByArea(area);
+        List<Map<String, String>> response = new ArrayList<>();
+
+        for (FieldWorker fieldWorker : fieldWorkers) {
+            int id = idMappingRepository.findByPrivateId(UUID.fromString(fieldWorker.getId())).getPublicId();
+            User user = fieldWorker.getUser();
+            Map<String, String> fieldWorkerDetails = new HashMap<>();
+            fieldWorkerDetails.put("publicId", String.valueOf(id));
+            fieldWorkerDetails.put("firstName", user.getFirstName());
+            fieldWorkerDetails.put("lastName", user.getLastName());
+            fieldWorkerDetails.put("email", user.getEmail());
+            response.add(fieldWorkerDetails);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
