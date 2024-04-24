20e0eecdc475a61505de121104a0f34e2eb14c69
kalyaniverma <kalyani.verma900@gmail.com>
2024-04-24 18:05:41 +0530

    Added Field worker team Repository

--- server/demo/src/main/java/com/example/demo/Repository/FieldWorkerRepository.java
+++ server/demo/src/main/java/com/example/demo/Repository/FieldWorkerRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Entity.FieldWorker;
-import com.example.demo.Entity.Supervisor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
---
+++ server/demo/src/main/java/com/example/demo/models/AssignGuidelineRequest.java
@@ -0,0 +1,9 @@
+package com.example.demo.models;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AssignGuidelineRequest {
+    private int id;
+    private String guidelines;
+}
