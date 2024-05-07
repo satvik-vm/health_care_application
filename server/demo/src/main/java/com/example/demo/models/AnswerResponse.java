bce57f9772727b8a2cdba1deee93b00b36a732d2
mukul1357 <Mukul.Gupta@iiitb.ac.in>
2024-05-08 03:17:47 +0530

    large string value handled

--- server/demo/src/main/java/com/example/demo/models/AnswerResponse.java
+++ server/demo/src/main/java/com/example/demo/models/AnswerResponse.java
@@ -1,12 +1,16 @@
 package com.example.demo.models;
 
+import jakarta.persistence.Lob;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class AnswerResponse {
     private String mcqAns;
+
+    @Lob
     private String subjAns;
+
     private int rangeAns;
     private int qid;
 }
